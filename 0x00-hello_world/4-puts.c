acf7cb576a992c90d76247046d817e411f863843
Stylhaut <nonamecraft01@gmail.com>
2022-04-07 12:03:14 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
+/*
+ * the entry point for a program
+ * no argument
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
