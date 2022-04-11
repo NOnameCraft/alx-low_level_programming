0011025e0cd307bbd377693968f67c8e2b57ef5e
Stylhaut <nonamecraft01@gmail.com>
2022-04-11 19:12:12 +0000

    cond and iter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int i, n;
+
+for (i = 122, n = 1; n < 27; n++, i--)
+
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
