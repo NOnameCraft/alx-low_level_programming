7adfa3253bde806a26c07087957e79ecb996a78a
Stylhaut <nonamecraft01@gmail.com>
2022-04-11 18:28:10 +0000

    cond and iter

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int i = 0;
+
+while (i < 10)
+
+{
+
+printf("%d\n",i);
+
+}
+
+return (0);
+
+}
