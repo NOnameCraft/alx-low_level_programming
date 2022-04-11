649430bbf5a5b29a065b17b3488f272431d6a8ea
Stylhaut <nonamecraft01@gmail.com>
2022-04-11 19:01:15 +0000

    cond and iter

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int n;
+int i;
+
+for (i = 48, n = 1; n <= 10; n++, i++) 
+
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
