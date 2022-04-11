702a2a538dd9eeba094ad87ccada81fbc218d3b6
Stylhaut <nonamecraft01@gmail.com>
2022-04-11 05:40:42 +0000

    cond or iter

---
+++ 0x01-variables_if_else_while/2-printf_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int i;
+int n;
+
+for (i = 97, n = 0; n < 26; i++, n++)
+
+{
+
+putchar(i);
+
+}
+
+return (0);
+}
