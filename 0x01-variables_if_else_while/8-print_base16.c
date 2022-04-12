85b5413de3a1992a383fc90a5d014a111a0c70e8
Stylhaut <nonamecraft01@gmail.com>
2022-04-12 02:19:50 +0000

    cond and iter

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int n;
+char a;
+
+for (n = 48; n <= 57; n++)
+
+{
+
+putchar(n);
+
+}
+
+for (a = 97; a < 103; a++)
+
+{
+
+putchar(a);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
