551c2514a7a28d7a68d6e7cd6fa15bdd52c17837
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 03:05:35 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+int i;
+
+for (i = 97; i <= 122; i++)
+
+{
+
+_putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
