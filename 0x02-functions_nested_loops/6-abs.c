3727bc869abfd2b4bf02bf9d72a083792b9233d4
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 13:42:05 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _abs - entry point
+ * *@r: input
+ *
+ * Return: absolute value
+ */
+
+int _abs(int r)
+
+{
+
+if (r >= 0)
+
+{
+
+return (r);
+}
+
+if ( r < 0)
+
+{
+
+return (-r);
+}
+
+}
+
+
