006686a9ac27c0ea089386800ebc97e9a036f1c4
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 03:40:10 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int _islower(int c)
+
+{
+
+if (c >= 97 && c <= 122)
+
+{
+
+return (1);
+
+}
+
+else
+
+return (0);
+
+}
+
