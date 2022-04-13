39cdaa6f4a0737bec1c7cfba1d25fd058352ceab
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 18:07:09 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - entry point
+ *
+ * Return: no value
+ */
+
+void times_table(void)
+
+{
+
+int i, j, s;
+
+for (i = 0; i <= 9; i++)
+
+{
+
+for (j = 0; j <= 9; j++)
+
+{
+
+s = i * j;
+_putchar(s);
+_putchar(',');
+_putchar(' ');
+
+}
+
+_putchar('\n');
+
+}
+
+}
