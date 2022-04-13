842567a15c4d975d01e397666d54405244fc6929
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 14:12:40 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point 
+ *
+ * @n: input
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int n)
+
+{
+
+int x;
+
+x = n % 10;
+
+return (x);
+
+}
