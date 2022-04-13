542f52dd202c044c533a4c3f4f43826b25376fb8
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 15:01:49 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Return: no value;
+ */
+
+void jack_bauer(void)
+
+{
+
+int i, j;
+
+for (i = 0; i <= 23; i++)
+
+{
+
+for (j = 0; j <= 59; j++)
+
+{
+
+if ( i < 10)
+
+{
+
+_putchar('0');
+
+}
+_putchar(i);
+_putchar(':');
+
+if (j < 0)
+
+{
+
+_putchar('0');
+
+}
+_putchar(j);
+
+}
+
+}
+
+}
+
+
