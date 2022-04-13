6f86acc1df22b209e35efcbadbe56bce95a822ff
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 03:23:12 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+int i, j;
+
+for (i = 0; i < 10; i++)
+
+{
+
+for (j = 97; j <= 122; j++)
+
+{
+
+_putchar(j);
+
+}
+
+}
+
+putchar('\n');
+
+}
