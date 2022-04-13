4d127ca887bbcbfb1093c9d4bec1783a5f06bde4
Stylhaut <nonamecraft01@gmail.com>
2022-04-13 02:51:52 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: value 0 to exit
+ */
+
+int main(void)
+
+{
+
+char text[9] = "_putchar";
+
+int i;
+
+for (i = 0; i < 9; i++)
+
+{
+
+_putchar(text[i]);
+
+}
+
+_putchar('\n');
+
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+intro functions and nested loops
