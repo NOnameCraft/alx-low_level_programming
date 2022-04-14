e61a3621c318a717c686e36e7b2521d074814f88
Stylhaut <nonamecraft01@gmail.com>
2022-04-14 14:45:27 +0000

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - entry point
+ *
+ * Return: case of 0
+ */
+
+int main(void)
+
+{
+
+int i;
+i = 0;
+
+positve_or_negative(i);
+
+return (0);
+
+}
+
+
