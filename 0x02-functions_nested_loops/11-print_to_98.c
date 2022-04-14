5a0f1ab629c7031b539adac5f89d000194822d71
Stylhaut <nonamecraft01@gmail.com>
2022-04-14 12:44:47 +0000

    funcs and loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry point
+ * @n: argument
+ * Return: no value
+ */
+void print_to_98(int n)
+
+{
+int i, j;
+if (n <= 0)
+
+{
+for (i = n; i <= 98; i++)
+
+{
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
+
+if (i != 98) 
+
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+
+if (n > 0)
+
+{
+for (i = n; i >= 98; i--)
+
+{
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
+if (i != 98)
+
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
