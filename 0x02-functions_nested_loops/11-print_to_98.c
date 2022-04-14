28705f04f0c47c31185eb169fb94db7da1a9f773
Stylhaut <nonamecraft01@gmail.com>
2022-04-14 14:03:57 +0000

    funcs and loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,45 +10,24 @@ void print_to_98(int n)
 
 {
 int i;
-
-if (n == 98)
-
-{
-printf("%d", n);
-}
-
-if (n < 98)
-
+if (n <= 98)
 {
 for (i = n; i <= 98; i++)
-
 {
-
-if (i ==98)
-
-printf("%d", i);
-
+if (i == 98)
+printf("%d\n", i);
 else
-
 printf("%d, ", i);
-
 }
 }
-
 if (n > 98)
-
 {
 for (i = n; i >= 98; i--)
-
 {
-
 if (i == 98)
-
-printf("%d", i);
+printf("%d\n", i);
 else
-
 printf("%d, ", i);
 }
-printf("\n");
 }
 }
