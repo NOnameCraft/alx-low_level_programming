0025f744899c3be6fe346d984bb53454b4d3b5e6
Stylhaut <nonamecraft01@gmail.com>
2022-04-15 21:24:36 +0000

    more on funcs and loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isdigit - entry point
+ * @c: argument
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+
+{
+
+if (c >= 48 && c <= 57)
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
