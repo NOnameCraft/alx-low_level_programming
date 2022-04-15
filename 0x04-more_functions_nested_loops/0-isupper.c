5fd4ba66e809648c5a7df95dc236071561ccf37e
Stylhaut <nonamecraft01@gmail.com>
2022-04-15 20:08:12 +0000

    more on funcs and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * is_upper - entry point
+ * @c: argument
+ * Return: 1 if c isppercase and 0 ohterwise
+ */
+
+int _ispper(int c)
+
+{
+
+if (c >= 65 && c <= 90)
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
