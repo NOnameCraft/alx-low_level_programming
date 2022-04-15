f5cdcf32b287e5abf3a8d7baf17e60b5e678438d
Stylhaut <nonamecraft01@gmail.com>
2022-04-15 23:47:57 +0000

    more on funcs and loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draw a line 
- * @n: number of time _ should be printed
+ * @n: number of time should be printed
  * Return: void
  */
 
@@ -12,10 +12,13 @@ void print_line(int n)
 	int i;
 	if (n <= 0)
 		_putchar('\n');
+	else
+	{
 	for (i = 0; i < n; i++)
-	
+
 	{
 		_putchar('_');
+	}
 	}
 	_putchar('\n');
 
