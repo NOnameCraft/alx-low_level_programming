b639c439d775c8442bcba53aaf02dddd72c63851
Stylhaut <nonamecraft01@gmail.com>
2022-04-15 22:11:10 +0000

    more on funcs and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a @b: arguments 
+ * Return: the produit of two numbes
+ */
+
+int mul(int a, int b)
+
+{
+
+int prod;
+
+prod = a * b;
+
+return (prod);
+
+}
