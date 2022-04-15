1304ff49dd2249d2e8ce3bd494c0b2b332123bef
Stylhaut <nonamecraft01@gmail.com>
2022-04-15 23:23:19 +0000

    more on funcs and loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,6 @@ void more_numbers(void)
 
 {
 int i, j;
-
 i = 0;
 while (i < 10)
 
@@ -19,15 +18,18 @@ j = 0;
 while (j < 15)
 
 {
-
 if (j >= 10)
-				
-_putchar('0' + j / 10);
-_putchar('0' + j % 10);
+
+_putchar('0' + (j / 10));
+_putchar('0' + (j % 10));
 j++;
 }
+
 _putchar('\n');
+
 i++;
+
 }
+
 }
 
