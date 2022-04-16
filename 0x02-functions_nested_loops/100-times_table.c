fc953ddc3f0ffa2b889df2cbe9d033b4006c613d
Stylhaut <nonamecraft01@gmail.com>
2022-04-16 03:56:25 +0000

    funcs and loop

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.H"
+/**
+ *print_times_table - n times table.
+ *@n: the number defines the table.
+ *
+ *Return: no return value
+ */
+void print_times_table(int n)
+{
+	int i, j, k,prod;
+	j = n + 1;
+	for (i = 0; i < j; i++)
+	{
+		for (k = 0; k < j; k++)
+		{
+			if (n > 15)
+			{
+				break;
+			}
+			else
+			{
+				prod = i * k;
+				if (k == 0)
+		       		{
+					_putchar('0' + prod);
+				}
+				else if (prod >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' +  (prod % 10));
+				}
+				else if (product >= 100)
+				{
+	       
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+				if (k != n)
+				{
+					_putchar(',');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
