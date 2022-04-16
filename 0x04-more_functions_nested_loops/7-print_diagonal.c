5566df7dad7b88e2dee8371638877a21d4cc7164
Stylhaut <nonamecraft01@gmail.com>
2022-04-16 00:09:23 +0000

    more on funcs and loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,22 @@
 void print_diagonal(int n)
 
 {
-	int i;
+	int i, j;
 	if(n <= 0)
+		
 		_putchar('\n');
 	else
+	
 	{
 		for (i = 0; i < n; i++)
+		
 		{
+			for (j = 0; j < i; j++)
+			
+			{
 			_putchar('\');
+			}
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
