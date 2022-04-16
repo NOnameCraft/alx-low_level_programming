ca48b16dd3aecfcf933e47a01e06aa7e243f308c
Stylhaut <nonamecraft01@gmail.com>
2022-04-16 01:57:18 +0000

    more on funcs and loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * print_square - print square with hastags
- * @size: number of the square
+ *print_square - prints square
+ *@size: size of square
  *
- * Return: void
+ *Return: void.
  */
 
 void print_square(int size)
 
 {
-
 	int i, j;
+
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
+
 	else
 	{
-		
-	        for (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-		        for (j = 0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
-			        _putchar(35);
+				_putchar('#');
 			}
-			
-		        _putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
