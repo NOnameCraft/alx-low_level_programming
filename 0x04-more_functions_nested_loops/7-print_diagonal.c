af92f441f6628ce9f4f6a184d3c60dc3b332e4b4
Stylhaut <nonamecraft01@gmail.com>
2022-04-16 00:39:14 +0000

    more on funcs and loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - print diagonal with \
- * @n: number of \
- * 
+ * print_diagonal - print diagonal with backslash
+ * @n: number of backslash
  * Return: void
  */
 
@@ -14,16 +13,14 @@ void print_diagonal(int n)
 	if(n <= 0)	
 		_putchar('\n');
 	else
-	
 	{
 		for (i = 0; i < n; i++)
-		
 		{
 			for (j = 0; j < i; j++)
-			
 			{
 			_putchar(' ');
 			}
+
 			_putchar(92);
 			_putchar('\n');
 		}
