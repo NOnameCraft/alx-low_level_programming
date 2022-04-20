c801c1747c1060966ed8f4890eebaf6d35fb9d34
Stylhaut <nonamecraft01@gmail.com>
2022-04-20 06:18:26 +0000

    pointers ..

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
  * print_rev - print string in reverse
@@ -11,10 +10,15 @@
 void print_rev(char *s)
 
 {
-	int i = _strlen(s);
-	while (i != 0)
+	int i = 0, j;
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
 		i++;
+	}
+	j = i;
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
 	}
 }
