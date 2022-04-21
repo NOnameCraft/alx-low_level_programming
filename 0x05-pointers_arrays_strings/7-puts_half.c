d19385dc608be612a42c44d15ecfaf6646e69939
Stylhaut <nonamecraft01@gmail.com>
2022-04-21 02:32:55 +0000

    pointers ..

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str: argument
+ *
+ *Return: void
+ */
+
+void puts_half(char *str)
+
+{
+	int n, i, mid;
+
+	length = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		mid = i / 2;
+		for (n = mid; n < i; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		mid = (i - 1) / 2;
+		for (n = mid + 1; n < i; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
+
