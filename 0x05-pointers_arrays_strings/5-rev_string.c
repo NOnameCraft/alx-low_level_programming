25f29a666693840d3fd2884e35acb533d0ce609d
Stylhaut <nonamecraft01@gmail.com>
2022-04-21 01:56:59 +0000

    pointers ..

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: argument
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+
+{
+	int i = 0, j = 0, k = 0, n = 0;
+	char *p = NULL;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	*p = "";
+	while (j >= 0)
+	{
+		p[k] += s[j];
+		j--;
+		k++;
+	}
+	while (p[n] != '\0')
+	{
+		_putchar(p[n]);
+		n++;
+	}
+	_putchar('\n');
+}
