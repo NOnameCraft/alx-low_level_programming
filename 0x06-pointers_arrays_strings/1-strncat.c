e80c1fd3e0eb6e0d156548e92bad122d9f944c6e
Stylhaut <nonamecraft01@gmail.com>
2022-04-24 04:50:02 +0000

    snd on pointers...

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,j;
+	int i = 0, j = 0;
 
-	i = 0;
-	j = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (j < n)
+	while (j < n && src[n] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
