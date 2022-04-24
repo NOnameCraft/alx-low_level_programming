a0437f7146974e2c991005b994f9120af4c09fbc
Stylhaut <nonamecraft01@gmail.com>
2022-04-24 03:47:28 +0000

    snd on pointers...

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,16 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-        while (src[i] != '\0')
+        while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (j < i)
+	while (src[j] != '\0')
 	{
-		dest[j] = src[j];
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (*dest);
