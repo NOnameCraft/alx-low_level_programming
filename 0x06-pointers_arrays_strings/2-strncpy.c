e6d301b433e6804165df50d9e52c5135743b5d46
Stylhaut <nonamecraft01@gmail.com>
2022-04-25 02:12:39 +0000

    snd on pointers...

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * strncpy - copy n character
+ * @dest: argument
+ * @src: argument
+ * @n: argument
+ *
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
