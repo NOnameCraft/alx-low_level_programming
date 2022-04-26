d210a0edc27a6b8673451e13df6c885b4c49501a
Stylhaut <nonamecraft01@gmail.com>
2022-04-26 02:38:36 +0000

    snd on pointers...

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-		for (; i < n; i++)
-			dest[i] = '\0';
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+		for (; j < n; j++)
+			dest[j] = '\0';
 
 	return (dest);
 }
