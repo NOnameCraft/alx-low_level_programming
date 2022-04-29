cc2445616a1706802147641cf5f813825872bb71
Stylhaut <nonamecraft01@gmail.com>
2022-04-29 02:16:51 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * _print_rev_recursion - that prints a string in reverse
  * @s: argument
  * Return: void
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
