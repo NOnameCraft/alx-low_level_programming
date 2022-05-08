0d480f0faf30d0ec35d923e189d00730c79ccc86
Stylhaut <nonamecraft01@gmail.com>
2022-05-08 02:12:23 +0000

    deep in memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,14 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * string_nconcat - concatenates two strings
- * @s1: argument
- * @s2: argument
- * @n: argument
- * 
- * Return: pointer shall point to a newly allocated space in memory
- * which contains s1 followed by the first n
- * bytes of s2, and null terminated
- */
-
+  * string_nconcat - concatenates two strings.
+  * @s1: pointer to first string.
+  * @s2: pointer to second string from which we'll take first n bytes.
+  * @n: n bytes of s2.
+  *
+  * Return: pointer to newly allocated space in memory.
+  * NULL if malloc fails.
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *nstr;
