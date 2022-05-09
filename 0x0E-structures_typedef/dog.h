e8b1e3bea24d10d887080fbd74d0f5609b4849dd
Stylhaut <nonamecraft01@gmail.com>
2022-05-09 15:04:37 +0000

    struct & typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - structure of a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of the dog
+ */
+
+struct dog{
+    char *name;
+    float age;
+    char *owner;
+};
