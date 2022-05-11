af65e66c6bac4b63edd75a320879fda3682c17ca
Stylhaut <nonamecraft01@gmail.com>
2022-05-11 00:16:34 +0000

    struct & typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,14 +6,12 @@
   *@name: first member
   *@age: second member
   *@owner: third member
-  *
   *Return: void
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
 	return;
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
