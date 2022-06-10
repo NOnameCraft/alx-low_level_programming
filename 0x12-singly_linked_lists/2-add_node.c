d6a5fb527fcae3a7c14ed8d3f8dc26ca27cae049
Stylhaut <nonamecraft01@gmail.com>
2022-06-10 11:01:55 +0000

    linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-#include <string.h>
+
 /**
- *add_node - function to add item at the beginning of a linked_list
- *@head: head pointer to linked_list
- *@str: string to add
- *
- *Return: address of new element
+ * add_node - adds new node at beginning of a list_t list
+ * @head: the node at the beginning of the list
+ * @str: the string to be contained in the new node
+ * Return: the pointer to the new node otherwise, NULL
  */
+
 list_t *add_node(list_t **head, const char *str)
+
 {
-list_t *new_node;
-unsigned int i = 0;
 
-if (str == NULL)
-return (NULL);
+unsigned int len = 0, size;
+	char *str1;
+	list_t *new_node;
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
-
-while (new_node->str[i])
-{
-i++;
-}
-new_node->len = i;
-new_node->next = *head;
-*head = new_node;
-
-return (new_node);
+	for (len = 0; str && str[len] != '\0'; len++)
+		;
+	if (head)
+	{
+		size = sizeof(list_t);
+		new_node = malloc(size);
+		if (new_node)
+		{
+			str1 = malloc(sizeof(char) * (len + 1));
+			if (!str1)
+			{
+				free(new_node);
+				return (NULL);
+			}
+			for (len = 0; str[len] != '\0'; len++)
+				str1[len] = str[len];
+			str1[len] = '\0';
+			new_node->str = str1;
+			new_node->len = len;
+			new_node->next = *head;
+			*head = new_node;
+		}
+	}
+	return (new_node);
 }
